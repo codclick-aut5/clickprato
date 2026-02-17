768b3ae494e89f87a4c83acb40bf69ff3177a8e1
codclick-aut5 <codclick.aut5@gmail.com>
2026-02-17 02:04:00 -0300

    Update restaurant header text for clarity

--- src/components/RestaurantHeader.tsx
+++ src/components/RestaurantHeader.tsx
@@ -32,10 +32,11 @@ const RestaurantHeader: React.FC = () => {
             <div className="text-center sm:text-left">
               <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">ClickPrato</h1>
               <p className="text-gray-600 mt-1"><b>Cardápio Digital com Inteligência Artificial</b></p>
-              <p className="text-gray-600 mt-1"><b>Pizzarias</b></p>
-              <p className="text-gray-600 mt-1"><b>Hamburguerias</b></p>
               <p className="text-gray-600 mt-1"><b>&nbsp;</b></p>
-              <p className="text-gray-600 mt-1"><b>Para todo tipo de Restaurante Delivery</b></p>
+              <p className="text-gray-600 mt-1"><b>Especial para Pizzarias</b></p>
+              <p className="text-gray-600 mt-1"><b>Ótimo para Hamburguerias</b></p>
+              <p className="text-gray-600 mt-1"><b>&nbsp;</b></p>
+              <p className="text-gray-600 mt-1"><b>Serve todo tipo de Restaurante Delivery</b></p>
               <p className="text-gray-600 mt-1"><b>&nbsp;</b></p>              
               <div className="flex items-center justify-center sm:justify-start mt-2">
                 <div className="flex text-yellow-400">
